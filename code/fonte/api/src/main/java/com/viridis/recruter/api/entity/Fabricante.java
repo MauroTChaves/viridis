86a0ab09c1533ac78f2c7c39e7cce3c8de157b25
Mauro Chaves <maurotchaves@gmail.com>
2018-04-15 20:53:52 -0300

    Criação da entidade Fabricante

---
+++ code/fonte/api/src/main/java/com/viridis/recruter/api/entity/Fabricante.java
@@ -0,0 +1,53 @@
+package com.viridis.recruter.api.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entidade que representa um fabricante de dispositivo elétrico
+ * @author mauro.chaves
+ *
+ */
+@Entity
+@Table(name = "fabricante")
+public class Fabricante {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_fabricante")
+	private Long id;
+	
+	@Column(name = "codigo_fabricante", length = 10, nullable = false)
+	private String codigo;
+	
+	@Column(name = "nome_fabricante", length = 50, nullable = false)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
