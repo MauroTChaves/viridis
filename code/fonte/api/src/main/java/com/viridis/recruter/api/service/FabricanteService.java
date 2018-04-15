4c269ef6f04e4686b5c64bd86800faf8a7512bc5
Mauro Chaves <maurotchaves@gmail.com>
2018-04-15 20:54:47 -0300

    Criação do serviço de fabricante

---
+++ code/fonte/api/src/main/java/com/viridis/recruter/api/service/FabricanteService.java
@@ -0,0 +1,20 @@
+package com.viridis.recruter.api.service;
+
+import java.util.List;
+
+import com.viridis.recruter.api.entity.Fabricante;
+
+public interface FabricanteService {
+	
+	/**
+	 *Retorna a lista de todos os fabricante cadastrados no database
+	 */
+	List<Fabricante> findAll();
+	
+	/**
+	 * 
+	 * @param id
+	 * @return Fabricante passando como parametro o id.
+	 */
+	Fabricante findById(Long id);
+}
---
+++ code/fonte/api/src/main/java/com/viridis/recruter/api/service/FabricanteServiceImpl.java
@@ -0,0 +1,50 @@
+package com.viridis.recruter.api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viridis.recruter.api.entity.Fabricante;
+import com.viridis.recruter.api.repository.FabricanteRepository;
+
+/**
+ * Implenetação dos métodos da interface de serviço
+ * @author benicio
+ *
+ */
+@Service
+public class FabricanteServiceImpl implements FabricanteService {
+
+	@Autowired
+	private FabricanteRepository fabricanteRepository;
+    
+	@Override
+	public List<Fabricante> findAll() {
+		List<Fabricante> listaFabricantes = new ArrayList<>();
+		try {
+			listaFabricantes = (List<Fabricante>) this.fabricanteRepository.findAll();
+			if(!listaFabricantes.isEmpty()) {
+				return listaFabricantes;
+			}else {
+				return null;
+			}
+		} catch (Exception e) {
+			// TODO: Fazer o tratamendo a exceção
+		}
+		return listaFabricantes;
+	}
+
+
+	@Override
+	public Fabricante findById(Long idFabricante) {
+		Fabricante fabricante = new Fabricante();
+		try {
+			fabricante = this.fabricanteRepository.findById(idFabricante);
+		} catch (Exception e) {
+			// TODO: Fazer o tratamendo a exceção
+		}
+		return fabricante;
+	}
+}
