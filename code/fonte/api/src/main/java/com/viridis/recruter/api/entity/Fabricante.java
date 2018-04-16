ed5510d3c27d98527dc776b619cb7d722094da46
Mauro Chaves <maurotchaves@gmail.com>
2018-04-16 00:15:17 -0300

    Ajuste na entidade de fabricante para implementar Serializable

--- code/fonte/api/src/main/java/com/viridis/recruter/api/entity/Fabricante.java
+++ code/fonte/api/src/main/java/com/viridis/recruter/api/entity/Fabricante.java
@@ -1,5 +1,7 @@
 package com.viridis.recruter.api.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,8 +16,10 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "fabricante")
-public class Fabricante {
+public class Fabricante implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id_fabricante")
