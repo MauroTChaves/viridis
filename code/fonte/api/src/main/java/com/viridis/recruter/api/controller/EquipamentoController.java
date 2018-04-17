9a2546b9eb8774451bd6df7dce2e6c16dfacaaa9
Mauro Chaves <maurotchaves@gmail.com>
2018-04-16 22:37:27 -0300

    Ajuste nos controllers

--- code/fonte/api/src/main/java/com/viridis/recruter/api/controller/EquipamentoController.java
+++ code/fonte/api/src/main/java/com/viridis/recruter/api/controller/EquipamentoController.java
@@ -4,95 +4,110 @@ import java.sql.SQLException;
 
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.viridis.recruter.api.entity.Equipamento;
 import com.viridis.recruter.api.entity.Equipamento;
 import com.viridis.recruter.api.service.EquipamentoService;
 
 /**
  * Controller da entidade de equipamento
+ * 
  * @author mauro.chaves
  *
  */
 @RestController
-@RequestMapping("api/equipamento/")
+@RequestMapping("api/equipamentos")
 public class EquipamentoController {
-		
+
 	@Autowired
-	private  EquipamentoService equipamentoService;
-	
+	private EquipamentoService equipamentoService;
+
 	/**
 	 * Retorna todos os equipamentos cadastrados na database
+	 * 
 	 * @return lista de Equipamentos
 	 */
-	@RequestMapping(value = "/findAll", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseStatus(HttpStatus.OK)
-    public Iterable<Equipamento> getTodosEquipamentos() {
-        return equipamentoService.findAll();
-    }
-	
-	@ResponseStatus(HttpStatus.OK)
-	@RequestMapping(value = "/{equipamentoId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Equipamento findById(@PathVariable(value = "equipamentoId") Long equipamentoId) {
-		return equipamentoService.findById(equipamentoId);
+	@GetMapping()
+	public Iterable<Equipamento> getTodosEquipamentos() {
+		return equipamentoService.findAll();
 	}
-	
-	@ResponseStatus(HttpStatus.OK)
-	@PostMapping(value = {"/novoEquipamento"})
+
+	/**
+	 * Método que retorna um equipamento pssando com parametro o id
+	 * 
+	 * @param equipamentoId
+	 * @return
+	 */
+	@GetMapping(value = "/{equipamentoId}")
+	public Equipamento findById(@PathVariable(value = "equipamentoId") Long equipamentoId) {
+		return equipamentoService.findOne(equipamentoId);
+	}
+
+	/**
+	 * Método que salvar um equipamento e por boas praticas retorna o status 200 qnd
+	 * sucesso e 401 qnd erro.
+	 * 
+	 * @param equipamento
+	 * @throws SQLException
+	 */
+	@PostMapping(value = { "/novoEquipamento" })
 	public void salvarEquipamento(@RequestBody Equipamento equipamento) throws SQLException {
 		try {
 			this.equipamentoService.salvarEquipamento(equipamento);
-        } catch (ServiceException e) {
-        	//TODO: Fazer tratamento de exceção
-        }
-		
+		} catch (ServiceException e) {
+			System.err.println(e.toString());
+		}
+
 	}
-	
-	//TODO : Erro no método de alteração
-	@ResponseStatus(HttpStatus.OK)
-    @RequestMapping(method = RequestMethod.PUT,
-    consumes = MediaType.APPLICATION_JSON_VALUE,
-    produces = MediaType.APPLICATION_JSON_VALUE)
+
+	/**
+	 * Método que faz o update de um equipamento salvo no database
+	 * 
+	 * @param equipamento
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Equipamento editarEquipamento(@RequestBody Equipamento equipamento) {
-		Equipamento equipamentoAlterado = this.equipamentoService.findById(equipamento.getId());
+		Equipamento equipamentoAlterado = this.equipamentoService.findOne(equipamento.getId());
 		try {
 			this.equipamentoService.salvarEquipamento(equipamentoAlterado);
 			return equipamentoAlterado;
-        } catch (ServiceException e) {
-        	//TODO: Fazer tratamento de exceção
-        }	
-		return equipamento;
-		
+		} catch (ServiceException e) {
+			System.err.println(e.toString());
+			return null;
+		}
+
 	}
-	
-	
-	@ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "/{equipamentoId}", method = RequestMethod.DELETE)
+
+	/**
+	 * Método que exclui um equipamento do database
+	 * 
+	 * @param equipamentoId
+	 * @return
+	 */
+	@DeleteMapping(value = "/{equipamentoId}")
 	public String deletarEquipamento(@PathVariable(value = "equipamentoId") Long equipamentoId) {
 		String statusDelacao = "";
 		try {
-			Equipamento equipamentoDeletado = this.equipamentoService.findById(equipamentoId);
-			if(equipamentoDeletado != null) {
+			Equipamento equipamentoDeletado = this.equipamentoService.findOne(equipamentoId);
+			if (equipamentoDeletado != null) {
 				this.equipamentoService.deletarEquipamento(equipamentoDeletado.getId());
 				statusDelacao = "Equipamento " + equipamentoDeletado.getDescricao() + " deletado com sucesso";
-			}else {
+			} else {
 				statusDelacao = "Equipamento de id " + equipamentoId + " não encontrado.";
 			}
 		} catch (Exception e) {
-			statusDelacao = "Erro ao deletar Equipamento";
+			statusDelacao = "Erro ao deletar Equipamento" + e.toString();
 		}
-		return  statusDelacao;
+		return statusDelacao;
 	}
-	
 
-	
 }
--- code/fonte/api/src/main/java/com/viridis/recruter/api/controller/FabricanteController.java
+++ code/fonte/api/src/main/java/com/viridis/recruter/api/controller/FabricanteController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,80 +20,92 @@ import com.viridis.recruter.api.entity.Fabricante;
 import com.viridis.recruter.api.service.FabricanteService;
 
 /**
- * Controller da entidade de fabricante que possui relacionamento com a entidade de equipamento
+ * Controller da entidade de fabricante que possui relacionamento com a entidade
+ * de equipamento
+ * 
  * @author mauro.chaves
  *
  */
 @RestController
-@RequestMapping("api/fabricante/")
+@RequestMapping("api/fabricantes")
 public class FabricanteController {
-		
+
 	@Autowired
-	private  FabricanteService fabricanteService;
-	
+	private FabricanteService fabricanteService;
+
 	/**
 	 * Retorna todos os fabricantes cadastrados na database
+	 * 
 	 * @return lista de Fabricantes
 	 */
-	@RequestMapping(value = "/findAll", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseStatus(HttpStatus.OK)
-    public Iterable<Fabricante> getTodosFabricantes() {
-        return fabricanteService.findAll();
-    }
-	
-	@ResponseStatus(HttpStatus.OK)
-	@RequestMapping(value = "/{fabricanteId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Fabricante findById(@PathVariable(value = "fabricanteId") Long fabricanteId) {
-		return fabricanteService.findById(fabricanteId);
+	@GetMapping()
+	public Iterable<Fabricante> getTodosFabricantes() {
+		return fabricanteService.findAll();
 	}
-	
-	@ResponseStatus(HttpStatus.OK)
-	@PostMapping(value = {"/novoFabricante"})
+
+	/**
+	 * Busca um fabricante pelo id
+	 * 
+	 * @param fabricanteId
+	 * @return
+	 */
+	@GetMapping(value = "/{fabricanteId}")
+	public Fabricante findById(@PathVariable(value = "fabricanteId") Long fabricanteId) {
+		return fabricanteService.findOne(fabricanteId);
+	}
+
+	/**
+	 * Método que inseri o fabricante do database
+	 * 
+	 * @param fabricante
+	 * @throws SQLException
+	 */
+	@PostMapping(value = { "/novoFabricante" })
 	public void salvarFabricante(@RequestBody Fabricante fabricante) throws SQLException {
 		try {
 			this.fabricanteService.salvarFabricante(fabricante);
-        } catch (ServiceException e) {
-        	//TODO: Fazer tratamento de exceção
-        }
-		
+		} catch (ServiceException e) {
+			System.err.println(e.getMessage());
+		}
+
 	}
-	
-	//TODO : Erro no método de alteração
+
+	// TODO : Erro no método de alteração
 	@ResponseStatus(HttpStatus.OK)
-    @RequestMapping(method = RequestMethod.PUT,
-    consumes = MediaType.APPLICATION_JSON_VALUE,
-    produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Fabricante editarFabricante(@RequestBody Fabricante fabricante) {
-		Fabricante fabricanteAlterado = this.fabricanteService.findById(fabricante.getId());
+		Fabricante fabricanteAlterado = this.fabricanteService.findOne(fabricante.getId());
 		try {
 			this.fabricanteService.salvarFabricante(fabricanteAlterado);
 			return fabricanteAlterado;
-        } catch (ServiceException e) {
-        	//TODO: Fazer tratamento de exceção
-        }	
+		} catch (ServiceException e) {
+			// TODO: Fazer tratamento de exceção
+		}
 		return fabricante;
-		
+
 	}
-	
-	
-	@ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "/{fabricanteId}", method = RequestMethod.DELETE)
+
+	/**
+	 * Método que exclui um fabricante da database
+	 * 
+	 * @param fabricanteId
+	 * @return
+	 */
+	@DeleteMapping(value = "/{fabricanteId}")
 	public String deletarFabricante(@PathVariable(value = "fabricanteId") Long fabricanteId) {
 		String statusDelacao = "";
 		try {
-			Fabricante fabricanteDeletado = this.fabricanteService.findById(fabricanteId);
-			if(fabricanteDeletado != null) {
+			Fabricante fabricanteDeletado = this.fabricanteService.findOne(fabricanteId);
+			if (fabricanteDeletado != null) {
 				this.fabricanteService.deletarFabricante(fabricanteDeletado.getId());
 				statusDelacao = "Fabricante " + fabricanteDeletado.getNome() + " deletado com sucesso";
-			}else {
+			} else {
 				statusDelacao = "Fabricante de id " + fabricanteId + " não encontrado.";
 			}
 		} catch (Exception e) {
 			statusDelacao = "Erro ao deletar fabricante";
 		}
-		return  statusDelacao;
+		return statusDelacao;
 	}
-	
 
-	
 }
