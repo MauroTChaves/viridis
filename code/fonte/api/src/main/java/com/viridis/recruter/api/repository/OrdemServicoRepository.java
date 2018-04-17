0937509795f3513ad112d4b9346a91d42b890b36
Mauro Chaves <maurotchaves@gmail.com>
2018-04-17 01:49:00 -0300

    Criação do repostitory de Ordem de Serviço

---
+++ code/fonte/api/src/main/java/com/viridis/recruter/api/repository/OrdemServicoRepository.java
@@ -0,0 +1,13 @@
+package com.viridis.recruter.api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.viridis.recruter.api.entity.OrdemServico;
+
+/**
+ * 
+ * @author mauro.chaves
+ *
+ */
+public interface OrdemServicoRepository extends CrudRepository<OrdemServico, Long> {
+}
