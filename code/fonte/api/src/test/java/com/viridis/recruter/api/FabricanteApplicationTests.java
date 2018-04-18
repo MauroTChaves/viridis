41c1e41127bd6a56a16e82267e3951d49e741336
Mauro Chaves <maurotchaves@gmail.com>
2018-04-18 04:12:03 -0300

    Deleção da classe FabricanteServiceTest.

---
+++ code/fonte/api/src/test/java/com/viridis/recruter/api/FabricanteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.viridis.recruter.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FabricanteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
